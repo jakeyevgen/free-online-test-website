33c8c530382d0082505df009d8282220e46dc5f3
jakeyevgen <jakeyevgen1984@gmail.com>
2023-04-25 11:29:01 +0300

    Update 12

---
+++ js/testjs.js
@@ -0,0 +1,134 @@
+// let userName = "Вася"
+// let user = "Вася"
+
+// console.log(userName)
+
+// let userHeight = 145 / 0;
+// console.log(userHeight)
+
+// let userName;
+// console.log(typeof userName);
+
+// let userSize = "45" / '8';
+// console.log(typeof userSize)
+
+// console.log("558" > 22)
+
+// let userCounter = 0;
+// let newUsers = userCounter++;
+// console.log(newUsers)
+
+// console.log(!false && 11 || 18 && !'')
+
+// let namee = 0;
+// console.log(namee ?? "jhjhj")
+
+// if (5 == '5') {
+//     console.log('Истина!')
+// } else {
+//     console.log('Ложь!')
+// }
+
+// let message = (92 > '11' && 58 < 100) ? 'Истина!' : 'Ложь!'
+// console.log(message)
+
+// if (0) {
+//     console.log('Ложь!')
+// } else if(" ") {
+//     console.log('Истина!')
+// }
+
+// const isComing = confirm("Please confirm hotel reservation");
+// console.log(isComing);
+
+// let clientCounter = 18;
+// const maxClients = 25;
+
+// while (clientCounter < maxClients) {
+//   console.log(clientCounter);
+//   clientCounter += 1;
+// }
+
+// let password = "";
+
+// do {
+//   password = prompt("Введіть пароль довший 4-х символів", "");
+// } while (password.length < 5);
+
+// console.log("Ввели пароль: ", password);
+
+// for (let i = 0; i <= 20; i += 5) {
+//   console.log(i);
+// }
+
+// const target = 3;
+// let sum = 0;
+
+// for (let i = 0; i <= target; i += 1) {
+//   sum += i;
+// }
+
+// console.log(sum);
+
+// const max = 10;
+// for (let i = 0; i < max; i += 1) {
+//   console.log(`${max} % ${i} = `, max % i);
+// }
+
+// for (let i = 0; i <= 5; i += 1) {
+//   console.log(i);
+
+//   if (i === 3) {
+//     console.log("Знайшли число 3, перериваємо виконання циклу");
+//     break;
+//   }
+// }
+
+// console.log("Лог після циклу");
+
+// const number = 10;
+
+// for (let i = 0; i < number; i += 1) {
+//   if (i % 2 === 0) {
+//     continue;
+//   }
+
+//   console.log("Непарне i: ", i); // 1, 3, 5, 7, 9
+// }
+
+// const fruits = ["Яблуко", "Груша", "Апельсин"];
+// const shoppingCart = fruits;
+// shoppingCart.push("Банан");
+// console.log(fruits.length);
+
+// for (let num = 0; num <= 5; num++) {
+//     console.log(num)
+// }
+
+// let num = 0;
+// while (num < 5) {
+//     console.log(num)
+//     num++;
+// }
+
+// let num = 0;
+// while (num < 5) console.log(num++)
+
+// let num = 0;
+// do {
+//     console.log(num);
+//     num++;
+// } while (num < 5)
+
+// for (num = 0; num <= 5; num++) {
+//     console.log(num)
+// }
+
+// let num = 8;
+// while (num) {
+//     console.log(num);
+//     num--;
+// }
+
+let num = 0;
+while (num < 3) console.log('Число: ${num++}');
